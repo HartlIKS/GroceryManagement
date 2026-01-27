a3d3aede88c2359529eda8a3d62557e0bcb4320f
Til Hartl <T.Hartl@iks-gmbh.com>
2026-01-27 16:32:30 +0100

    fix address formatting

--- frontend/src/app/components/stores/store-list.component.ts
+++ frontend/src/app/components/stores/store-list.component.ts
@@ -81,4 +81,34 @@ export class StoreListComponent implements OnInit {
       });
     }
   }
+
+  formatAddress(address: any): string {
+    if (!address) {
+      return '-';
+    }
+    
+    const parts: string[] = [];
+    
+    if (address.street && address.number) {
+      parts.push(`${address.street} ${address.number}`);
+    } else if (address.street) {
+      parts.push(address.street);
+    } else if (address.number) {
+      parts.push(address.number);
+    }
+    
+    if (address.zip && address.city) {
+      parts.push(`${address.zip} ${address.city}`);
+    } else if (address.zip) {
+      parts.push(address.zip);
+    } else if (address.city) {
+      parts.push(address.city);
+    }
+    
+    if (address.country) {
+      parts.push(address.country);
+    }
+    
+    return parts.join(', ');
+  }
 }
