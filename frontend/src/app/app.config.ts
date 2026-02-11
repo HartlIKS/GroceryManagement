019a270809f6c988a08d5e70d272a23f633b0b6a
Til Hartl <T.Hartl@iks-gmbh.com>
2026-02-11 09:29:21 +0100

    removed unused ngrx dependency.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,13 +3,11 @@ import { provideRouter, withDebugTracing } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withDebugTracing()),
     provideHttpClient(),
-    provideStore()
 ]
 };
