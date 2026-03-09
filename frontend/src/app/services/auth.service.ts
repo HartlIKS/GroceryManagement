1961bc4a549d924bfd89aad7752148e3bda02c2b
Til Hartl <T.Hartl@iks-gmbh.com>
2026-03-09 12:37:29 +0100

    allow fetching scopes from access token

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -4,6 +4,23 @@ import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 
+function getClaims(token?: string) {
+  if(token === undefined) return undefined;
+  const dat = token.split('.')[1];
+  if(dat === undefined) return undefined;
+  const b64 = dat.padEnd(Math.ceil(dat.length / 4) * 4, '=').replace(/-/g, '+').replace(/_/g, '/');
+
+  const json =  decodeURIComponent(
+    atob(b64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+  return JSON.parse(json);
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -13,8 +30,8 @@ export class AuthService {
   readonly accessToken = this.accessToken_.asReadonly();
   readonly settle: Promise<void>;
   readonly scopes = computed(() => {
-    this.accessToken();
-    return this.oauthService.getGrantedScopes() as string[] ?? [];
+    const token = this.accessToken();
+    return this.oauthService.getGrantedScopes() as string[] ?? getClaims(token)?.['scope']?.split(' ') ?? [];
   })
   readonly hasMasterData = computed(() => this.scopes().includes('MASTERDATA'))
   readonly claims = computed((): Record<string, any> | undefined => {
