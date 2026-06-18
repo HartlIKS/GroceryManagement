24af266575eadc3dc0010db1e981955b22b6b38a
Til Hartl <T.Hartl@iks-gmbh.com>
2026-06-18 16:11:57 +0200

    update to angular 22

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
@@ -9,7 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+    provideHttpClient(),
     provideOAuthClient(),
   ],
 };
