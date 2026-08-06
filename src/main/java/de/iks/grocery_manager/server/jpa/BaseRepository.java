2f44c43eed7084cb5f76ecdf98993f668d0db5f4
Til Hartl <T.Hartl@iks-gmbh.com>
2026-08-06 16:35:47 +0200

    allow non-admins access to mass query endpoints

--- src/main/java/de/iks/grocery_manager/server/jpa/BaseRepository.java
+++ src/main/java/de/iks/grocery_manager/server/jpa/BaseRepository.java
@@ -3,7 +3,9 @@ package de.iks.grocery_manager.server.jpa;
 import de.iks.grocery_manager.server.model.HasUUID;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 
+import java.util.Collection;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 public interface BaseRepository<E extends HasUUID> extends PanacheRepositoryBase<E, UUID> {
     default E saveAndFlush(E entity) {
@@ -13,5 +15,9 @@ public interface BaseRepository<E extends HasUUID> extends PanacheRepositoryBase
 
     default boolean existsById(UUID id) {
         return findByIdOptional(id).isPresent();
+    }
+
+    default Stream<E> streamByIds(Collection<? extends UUID> ids) {
+        return stream("uuid in ?1", ids);
     }
 }
